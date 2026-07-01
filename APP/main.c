254822d4f4cd6e8b96ef7995538a7409dd0937e6
mohamedsabertawfik <mohamedaxa2@gmail.com>
2026-07-02 00:02:54 +0300

    v0

---
+++ APP/main.c
@@ -0,0 +1,18 @@
+/*
+ * ATmega32_BareMetal.c
+ *
+ * Created: 17/01/48 12:00:49 ص
+ * Author : moham
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
